43fdaf0be0ae567b2fee3a8361c4438ce519de28
github_DASorescu <srsdln97@gmail.com>
2022-09-19 18:00:18 +0200

    bonus-added-table-format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('doc-title','Movies')
 @section('main-content')
 <div class="container">
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-4">
             @foreach($movies as $movie)
             <div class="card">
@@ -13,7 +13,31 @@
             </div>
             @endforeach
         </div>
-    </div>
+    </div> --}}
+    <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Original Title</th>
+            <th scope="col">Nationality</th>
+            <th scope="col">Relase date</th>
+            <th scope="col">Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($movies as $movie)
+          <tr>
+            <th scope="row">{{$movie['id']}}</th>
+            <td>{{$movie['title']}}</td>
+            <td>{{$movie['original_title']}}</td>
+            <td>{{$movie['nationality']}}</td>
+            <td>{{$movie['date']}}</td>
+            <td>{{$movie['vote']}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
 </div>
 
 @endsection
